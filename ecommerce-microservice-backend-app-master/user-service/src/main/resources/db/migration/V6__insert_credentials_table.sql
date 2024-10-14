a3c722a311d89a0e4ea6a35049f93425defe0fb6
Siddhi <siddhidudhale18@gmail.com>
2024-10-14 23:49:32 +0600

    Microservice Project Commited

---
+++ ecommerce-microservice-backend-app-master/user-service/src/main/resources/db/migration/V6__insert_credentials_table.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO credentials
+(user_id, username, password, role, is_enabled) VALUES
+(1, 'selimhorri', '$2a$12$kc.PXrDXd9xTvysTIvGQyuwyAkLCAAJGbJ4f4QOhw8J6gIwNZ2MiO', 'ROLE_USER', true),
+(2, 'amineladjimi', '$2a$12$kc.PXrDXd9xTvysTIvGQyuwyAkLCAAJGbJ4f4QOhw8J6gIwNZ2MiO', 'ROLE_USER', true),
+(3, 'omarderouiche', '$2a$12$kc.PXrDXd9xTvysTIvGQyuwyAkLCAAJGbJ4f4QOhw8J6gIwNZ2MiO', 'ROLE_USER', true),
+(4, 'admin', '$2a$12$kc.PXrDXd9xTvysTIvGQyuwyAkLCAAJGbJ4f4QOhw8J6gIwNZ2MiO', 'ROLE_USER', true);
+
